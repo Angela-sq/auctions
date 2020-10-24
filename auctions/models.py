ff7f699cc6004feaef819d64a35bf3ab37ef9d14
Angela-sq <angela.sq.liu@gmail.com>
2020-10-24 13:58:32 -0700

    Fix starting bid.

--- auctions/models.py
+++ auctions/models.py
@@ -1,17 +1,19 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# model for users
 class User(AbstractUser):
     username = models.CharField(max_length=64, unique=True)
     email = models.CharField(max_length=64, unique=True)
     password = models.CharField(max_length=64)
 
+# model for listings
 class Listing(models.Model):
     seller = models.CharField(max_length=64)
     title = models.CharField(max_length=64)
     description = models.TextField()
-    category = models.CharField(max_length=64)
+    category = models.CharField(max_length=64, default=None)
     starting_bid = models.IntegerField(default=0)
     URL = models.CharField(
         max_length=200, default=None)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
